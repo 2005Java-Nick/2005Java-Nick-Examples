302dc85d836826d994a20151a2999870b9ac35b6
jurczakn <nickolas.jurczak@gmail.com>
2020-05-04 15:28:50 -0400

    added hello world

---
+++ Week1/FirstJavaProject/src/com/revature/Driver.java
@@ -0,0 +1,26 @@
+package com.revature;
+
+/*
+ * Naming Conventions
+ *	Class: typically a none
+ *		Pascal Casing (capital first letter of each word, no spaces)
+ *  Interfaces: typically and adjective
+ *  	Pascal Casing
+ *  Methods: Camel Casing (first letter lower case, capital for each additional word, no spaces)
+ *  Variables: Camel Casing
+ *  Constants: ALL CAPS, with _ between words
+ *  Package: lower case, reverse domain name
+ */
+
+public class Driver {
+	
+	private String MyValue = "";
+	
+	//Main method: only method JVM executes, entrypoint to application
+	static public void main(String pirateSounds[]) {
+		System.out.println("Hello World");
+	}
+	
+	public void DO_STUFF() {}
+
+}
