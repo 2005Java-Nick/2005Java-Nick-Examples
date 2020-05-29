f79d81a7ff0f18db377bbf902773bc00800254a4
Andres Toledo <49215828+AndresMToledo@users.noreply.github.com>
2020-05-29 15:56:03 -0400

    blackjack with login service not working

--- Week4/blackjack-ng/src/app/component/login/login.component.ts
+++ Week4/blackjack-ng/src/app/component/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private username: string;
+  private password: string;
+
+  authenticate()
+  {
+    this.authenticationService.authenticate(this.username, this.password);
+  }
+
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
